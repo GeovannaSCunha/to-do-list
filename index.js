e7d52bc6e9d4b1f5116d2d912aeac851cbb67c8a
GeovannaSCunha <geovanna.scunha@gmail.com>
2025-04-16 10:55:46 -0300

    inserindo o botão de excluir

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
 import verificarListaVazia from "./scripts/verificarListaVazia.js";
+import { apagarItemdaLista} from "./scripts/apagarItemdaLista.js";
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
@@ -11,5 +12,9 @@ botaoAdicionar.addEventListener("click", (evento) => {
     verificarListaVazia(listaDeCompras);
 })
 
+botaoExcluir.addEventListener("click", (evento) =>{
+    evento.preventDefault();
+    const apagar = apagarItemdaLista();
+})
 
 verificarListaVazia(listaDeCompras);
